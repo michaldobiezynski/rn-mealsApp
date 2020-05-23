dc6e28e6c9d799d62dafbe7a9961a95556453228
michaldobiezynski <dobiezynski@gmail.com>
2020-05-23 14:01:43 +0100

    adding tabs-based navigation

--- navigation/MealsNavigation.js
+++ navigation/MealsNavigation.js
@@ -9,6 +9,7 @@ import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
 import Colors from "../constants/Colors";
+import FavoritesScreen from "../screens/FavoritesScreen";
 
 const MealsNavigator = createStackNavigator(
   {
@@ -31,4 +32,9 @@ const MealsNavigator = createStackNavigator(
   },
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+  Meals: MealsNavigator,
+  Favorites: FavoritesScreen,
+});
+
+export default createAppContainer(MealsFavTabNavigator);
